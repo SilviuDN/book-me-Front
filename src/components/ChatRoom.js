30679c33cf4e7975697980665ba8c467ba0c72db
GianLucas <gianspf@gmail.com>
2021-08-05 18:15:54 +0200

    GianLucas - ChatRoom

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -1,100 +1,116 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import useChat from "../useChat";
-import SpinningBook from './RotateBook';
-import '../App.css';
+import SpinningBook from "./RotateBook";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Icon from "@material-ui/core/Icon";
+import { makeStyles } from "@material-ui/core/styles";
+import "../App.css";
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+}));
+
 function ChatRoom(props) {
-    const { roomId } = props.match.params; // Gets roomId from URL
-    const { messages, sendMessage } = useChat(roomId); // Creates a websocket and manages messaging
-    const [newMessage, setNewMessage] = useState(""); // Message to be sent
+  const classes = useStyles();
 
-    const [spinningDirection, setDirection] = useState(true)
+  const { roomId } = props.match.params; // Gets roomId from URL
 
-    const handleNewMessageChange = (event) => {
-        setNewMessage(event.target.value);
-    };
+  const { messages, sendMessage } = useChat(roomId); // Creates a websocket and manages messaging
+  const [newMessage, setNewMessage] = useState(""); // Message to be sent
 
-    const handleSendMessage = () => {
-        sendMessage(newMessage);
-        setNewMessage("");
-      };
+  const [spinningDirection, setDirection] = useState(true);
 
-      const [book, setBook] = useState([]);
-      useEffect( () => {
-        (async function Request() {
-          const res = await axios.get(
-            `https://www.googleapis.com/books/v1/volumes/${roomId}`
-            // `https://www.googleapis.com/books/v1/volumes?q="${roomId}+intitle:keyes"`
-            // `https://www.googleapis.com/books/v1/volumes?q="${roomId}"`
-            // `https://www.googleapis.com/books/v1/volumes?q=harry`
-          );
-          // console.log("From the chatRoom:", res.data)
-          setBook(res.data);
-        })()
-      }, [newMessage])
+  const handleNewMessageChange = (event) => {
+    setNewMessage(event.target.value);
+  };
 
+  const handleSendMessage = () => {
+    sendMessage(newMessage);
+    setNewMessage("");
+  };
 
-      const changeDirection = () =>{
-        const infoUrl = book.volumeInfo.infoLink
-        console.log(infoUrl)
-        setDirection(!spinningDirection)
-        window.open(`${infoUrl}`, "_blank")
-        
-      }
+  const [book, setBook] = useState([]);
+  useEffect(() => {
+    (async function Request() {
+      const res = await axios.get(
+        `https://www.googleapis.com/books/v1/volumes/${roomId}`
+        // `https://www.googleapis.com/books/v1/volumes?q="${roomId}+intitle:keyes"`
+        // `https://www.googleapis.com/books/v1/volumes?q="${roomId}"`
+        // `https://www.googleapis.com/books/v1/volumes?q=harry`
+      );
+      // console.log("From the chatRoom:", res.data)
+      setBook(res.data);
+    })();
+  }, [newMessage]);
 
+  const changeDirection = () => {
+    const infoUrl = book.volumeInfo.infoLink;
+    console.log(infoUrl);
+    setDirection(!spinningDirection);
+    window.open(`${infoUrl}`, "_blank");
+  };
 
-
-      return (
-        <div className="chat-room-container">
-          
-
+  return (
+    <div className="chat-room-container">
+      <div className="SpinTitle">
         <div onClick={() => changeDirection()}>
-          <SpinningBook  spinningDirection={spinningDirection} book={book}/>        
+          <SpinningBook spinningDirection={spinningDirection} book={book} />
         </div>
-        
-          <h1 className="room-name">Room: {book.volumeInfo?.title}</h1>
-          <div className="messages-container">
-            <ol className="messages-list">
-              {messages.map((message, i) => (
-                <li
-                  key={i}
-                  className={`message-item ${
-                    message.ownedByCurrentUser ? "my-message" : "received-message"
-                  }`}
-                >
-                  {message.body}
-                </li>
-              ))}
-            </ol>
-          </div>
-          <div className="new-message-input-field">
-            <textarea
+
+        <h1 className="room-name">Room: {book.volumeInfo?.title}</h1>
+      </div>
+
+      <div className="messages-container">
+        <ol className="messages-list">
+          {messages.map((message, i) => (
+            <li
+              key={i}
+              className={`message-item ${
+                message.ownedByCurrentUser ? "my-message" : "received-message"
+              }`}
+            >
+              {message.body}
+            </li>
+          ))}
+        </ol>
+      </div>
+      <div className="new-message-input-field">
+        {/* <input
               value={newMessage}
               onChange={handleNewMessageChange}
               placeholder="Write message..."
               className='new-message-textarea'
-            />
-            <button onClick={handleSendMessage} className="send-message-button">
+            /> */}
+
+        <TextField
+          id="outlined-search"
+          label="Menssage"
+          variant="outlined"
+          onChange={handleNewMessageChange}
+          value={newMessage}
+          minlength="1"
+          type="text"
+          style={{ width: "80%" }}
+        />
+        {/* <button onClick={handleSendMessage} className="send-message-button">
               Send
-            </button>
-          </div>
-        </div>
-      );
+            </button> */}
+
+        <Button
+          onClick={handleSendMessage}
+          variant="contained"
+          color="primary"
+          className={classes.button}
+          endIcon={<Icon>send</Icon>}
+        >
+          Send
+        </Button>
+      </div>
+    </div>
+  );
 }
-export default ChatRoom
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export default ChatRoom;
